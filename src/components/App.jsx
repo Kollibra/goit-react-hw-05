3043c1c3c4ff9533ba0ed9c99ab908476f493cfe
Iryna Kovalova <kovra777@gmail.com>
2024-03-09 23:01:34 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-
+// import { useState } from 'react';
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Loader from './Loader/Loader';
 import Navigation from './Navigation/Navigation';
-
-import HomePage from '../pages/HomePage/HomePage.jsx';
+import './App.module.css';
+// import HomePage from '../pages/HomePage/HomePage.jsx';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-// const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
 const MovieCast = lazy(() => import('./MovieCast/MovieCast'));
 const MovieReviews = lazy(() => import('./MovieReviews/MovieReviews'));
