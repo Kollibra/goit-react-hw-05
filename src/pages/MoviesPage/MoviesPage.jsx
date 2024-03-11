f4e5b66bbdecbdb6f5ef1e723c2f5ce8b8ac1ee2
Iryna Kovalova <kovra777@gmail.com>
2024-03-11 15:46:41 +0200

    Update MoviesPage.jsx

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,58 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+// import SearchBar from '../../components/SearchBar/SearchBar.jsx';
+// import { getFilmsByQuery } from '../../components/API/API.js';
+// import MovieList from '../../components/MovieList/MovieList.jsx';
+// import Loader from '../../components/Loader/Loader.jsx';
+// import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.jsx';
+// import { useSearchParams } from 'react-router-dom';
+
+// const MoviesPage = () => {
+//   // const location = useLocation();
+
+//   const [films, setFilms] = useState([]);
+//   const [error, setError] = useState(false);
+//   const [loading, setLoading] = useState(false);
+
+//   const [params, setParams] = useSearchParams();
+//   const search = params.get('search') ?? '';
+
+//   const changeSearch = newSearch => {
+//     params.set('search', newSearch);
+//     setParams(params);
+//   };
+
+//   const searchFilms = async query => {
+//     try {
+//       setError(false);
+//       setLoading(true);
+//       const response = await getFilmsByQuery(query);
+//       setFilms(response);
+//     } catch (error) {
+//       setError(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+//   useEffect(() => {
+//     if (search.trim() !== '') {
+//       searchFilms(search);
+//     }
+//   }, [search]);
+
+//   return (
+//     <div>
+//       {!loading && <SearchBar onSearch={changeSearch} value={search} />}
+//       {error && <ErrorMessage />}
+//       {films.length > 0 && <MovieList moviesList={films} />}
+//       {loading && <Loader />}
+//       <>{!films && !loading && <MovieList items={films} />}</>
+//     </div>
+//   );
+// };
+
+// export default MoviesPage;
+
+import { useState, useEffect, useCallback } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar.jsx';
 import { getFilmsByQuery } from '../../components/API/API.js';
 import MovieList from '../../components/MovieList/MovieList.jsx';
@@ -7,8 +61,6 @@ import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.jsx';
 import { useSearchParams } from 'react-router-dom';
 
 const MoviesPage = () => {
-  // const location = useLocation();
-
   const [films, setFilms] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -21,7 +73,7 @@ const MoviesPage = () => {
     setParams(params);
   };
 
-  const searchFilms = async query => {
+  const searchFilms = useCallback(async (query) => {
     try {
       setError(false);
       setLoading(true);
@@ -32,12 +84,13 @@ const MoviesPage = () => {
     } finally {
       setLoading(false);
     }
-  };
+  }, []);
+
   useEffect(() => {
     if (search.trim() !== '') {
       searchFilms(search);
     }
-  }, [search]);
+  }, [search, searchFilms]);
 
   return (
     <div>
